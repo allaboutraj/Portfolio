39c51499f4425c6171b1087c27bf0a9f5981fb2b
allaboutraj <rajgupta9507321@gmail.com>
2024-04-08 18:34:26 +0530

    Error fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoCloseSharp } from "react-icons/io5";
 
 const Navbar = () => {
+
   return (
     <>
       <div className="nav_bar container"
@@ -8,6 +11,34 @@ const Navbar = () => {
       data-aos-duration="1000"
       >
         <div className="left nav_items">PortFolio</div>
+        {/* Hamberger Designing */}
+        {/* <div className = "dropdown-menu text-right">
+                <div className="bars">
+                  <GiHamburgerMenu />
+                </div>
+                <div className ="dropdown-list nav-menu">
+                <IoCloseSharp />
+                    <ul className = "no-list-style">
+                        <li >
+                            <a href="#"> Home </a>
+                        </li>
+                        <li>
+                            <a href="#experience">Experience</a>
+                        </li>
+                        <li>
+                            <a href="#skills"> Skills </a>
+                        </li>
+                        <li>
+                            <a href="#projects">Projects</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                    </div>
+             </div> */}
+            {/* Hamberger Design Over */}
+
         <div className="right">
             <a href="#home" className="nav_items">Home</a>
             <a href="#experience" className="nav_items">Experience</a>
