569a5070a9691921b32cf526889e1b85b7ee961c
allaboutraj <rajgupta9507321@gmail.com>
2024-04-08 15:07:02 +0530

    Feature: Added Project Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
-
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Home />
         <Experience />
         <Skills/>
+        <Projects/>
       </div>
     </>
   )
